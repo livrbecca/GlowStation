a8dad82e74993c0dfa84027ebd8ea4b1e27fd34f
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-11-14 21:01:22 +0000

    filter button directions

--- glowstation/src/components/Shop.js
+++ glowstation/src/components/Shop.js
@@ -8,41 +8,48 @@ const Shop = (props) => {
   return (
     <div>
       <div className="buttonArea">
-        {["right"].map((direction) => (
           <DropdownButton
             className="mainButton"
             as={ButtonGroup}
-            key={direction}
-            id={`dropdown-button-drop-${direction}`}
-            drop={direction}
+            key={"down"}
+            id={`dropdown-button-drop-${"down"}`}
+            drop={"down"}
             title="Filter Products"
           >
             <Dropdown.Header className="filterBy">Filter by:</Dropdown.Header>
+
+        
             <DropdownButton
               title="Skin Concern"
-              className="mainButton"
+              className=""
               as={ButtonGroup}
-              key={direction}
-              drop={direction}
+              key={"right"}
+              drop={"right"}
             >
+           
               <Dropdown.Item eventKey="1">Acne & Blackheads</Dropdown.Item>
               <Dropdown.Item eventKey="2">Texture</Dropdown.Item>
               <Dropdown.Item eventKey="3">Eye concerns</Dropdown.Item>
               <Dropdown.Item eventKey="4">Lines & Wrinkes</Dropdown.Item>
               <Dropdown.Item eventKey="5">Dehydrated Skin</Dropdown.Item>
-              <Dropdown.Item eventKey="6">Hyperpigmentation & Brigtening</Dropdown.Item>
+              <Dropdown.Item eventKey="6">
+                Hyperpigmentation & Brigtening
+              </Dropdown.Item>
               <Dropdown.Item eventKey="7">Redness</Dropdown.Item>
-              <Dropdown.Item eventKey="8">Visible Pores & Oily Skin</Dropdown.Item>
+              <Dropdown.Item eventKey="8">
+                Visible Pores & Oily Skin
+              </Dropdown.Item>
               <Dropdown.Item eventKey="9">Dry Skin</Dropdown.Item>
             </DropdownButton>
+
             <DropdownButton
+              className=""
               title="Product Type"
-              className="mainButton"
               as={ButtonGroup}
-              key={direction}
-              drop={direction}
+              key={"right"}
+              drop={"right"}
             >
-               <Dropdown.Item eventKey="10">Cleansers</Dropdown.Item>
+              <Dropdown.Item eventKey="10">Cleansers</Dropdown.Item>
               <Dropdown.Item eventKey="11">Toners & Essence</Dropdown.Item>
               <Dropdown.Item eventKey="12">Mists</Dropdown.Item>
               <Dropdown.Item eventKey="13">Exfoliators</Dropdown.Item>
@@ -54,30 +61,29 @@ const Shop = (props) => {
             </DropdownButton>
             <DropdownButton
               title="Skin Type"
-              className="mainButton"
+              className=""
               as={ButtonGroup}
-              key={direction}
-              drop={direction}
+              key={"right"}
+              drop={"right"}
             >
-               <Dropdown.Item eventKey="19">Sensitive</Dropdown.Item>
+              <Dropdown.Item eventKey="19">Sensitive</Dropdown.Item>
               <Dropdown.Item eventKey="20">Oily</Dropdown.Item>
               <Dropdown.Item eventKey="21">Dry</Dropdown.Item>
               <Dropdown.Item eventKey="22">Combination</Dropdown.Item>
             </DropdownButton>
             <DropdownButton
               title="Brands"
-              className="mainButton"
+              className=""
               as={ButtonGroup}
-              key={direction}
-              drop={direction}
+              key={"right"}
+              drop={"right"}
             >
-               <Dropdown.Item eventKey="23">PSA Skin</Dropdown.Item>
+              <Dropdown.Item eventKey="23">PSA Skin</Dropdown.Item>
               <Dropdown.Item eventKey="24">Indie Lee</Dropdown.Item>
               <Dropdown.Item eventKey="25">The Inkey List</Dropdown.Item>
               <Dropdown.Item eventKey="26">COSRX</Dropdown.Item>
             </DropdownButton>
           </DropdownButton>
-        ))}
       </div>
       <div className="shopContainer">{props.products}</div>
     </div>
