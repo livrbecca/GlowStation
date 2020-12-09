df4a2ea0bf96571792354cee907026dabbde2180
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-12-09 20:32:17 +0000

    first home page advert carousel

--- glowstation/src/components/Home.js
+++ glowstation/src/components/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "../css/Home.css";
-
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <div className="welcomeMessage">
           <h1 style={{ textTransform: "capitalize" }}>Welcome {props.name}</h1>
-      <button>Click me</button>
+      <Link to="/loading2">
+        <AliceCarousel
+          animationDuration="600"
+          autoPlay
+          disableButtonsControls="true"
+          autoPlayInterval="2000"
+          animationType="fadeout"
+          infinite="true"
+        >
+          <img className="advert1" src="advert2.png" alt="advert" />
+          <img className="advert1" src="advert.png" alt="advert" />
+          <img className="advert1" src="advert3.png" alt="advert" />
+        </AliceCarousel>
+      </Link>
     </div>
   );
 };
