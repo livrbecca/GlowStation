610877579787e02ddb8e2529063a7a07210b28b6
livrbecca <69110329+livrbecca@users.noreply.github.com>
2020-12-18 00:05:17 +0000

    added icons to results and filter components

--- glowstation/src/components/Sidebar.js
+++ glowstation/src/components/Sidebar.js
@@ -5,10 +5,22 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
-
 const Sidebar = (props) => {
   return (
     <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-funnel-fill"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"
+        />
+      </svg>
       <DropdownButton
         className="mainButton"
         as={ButtonGroup}
