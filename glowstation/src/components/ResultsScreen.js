402df9f2573b308bb6672b363bddb904c975e203
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-01-13 15:25:21 +0000

    mapped through results screen

--- glowstation/src/components/ResultsScreen.js
+++ glowstation/src/components/ResultsScreen.js
@@ -45,112 +45,158 @@ const ResultsScreen = ({
               </Fragment>
             );
           })}
-
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP TWO - Optional</p>
-          <p>{mask.data[0].name}</p>
-          <p>£{mask.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={mask.data[0].imageLinks.img1}
-            alt={mask.data[0].name}
-          />
-          <p className="iconDes">{mask.data[0].description.smallDes}</p>
+          {mask.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP THREE</p>
-          <p>{toner.data[0].name}</p>
-          <p>£{toner.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={toner.data[0].imageLinks.img1}
-            alt={toner.data[0].name}
-          />
-          <p className="iconDes">{toner.data[0].description.smallDes}</p>
-
+          {toner.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP FOUR</p>
-          <p>{exfoliator.data[0].name}</p>
-          <p>£{exfoliator.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={exfoliator.data[0].imageLinks.img1}
-            alt={exfoliator.data[0].name}
-          />
-          <p className="iconDes">{exfoliator.data[0].description.smallDes}</p>
+          {exfoliator.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP FIVE - Optional</p>
-          <p>{mist.data[0].name}</p>
-          <p>£{mist.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={mist.data[0].imageLinks.img1}
-            alt={mist.data[0].name}
-          />
-          <p className="iconDes">{mist.data[0].description.smallDes}</p>
+          {mist.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP SIX</p>
-          <p>{serum.data[0].name}</p>
-          <p>£{serum.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={serum.data[0].imageLinks.img1}
-            alt={serum.data[0].name}
-          />
-          <p className="iconDes">{serum.data[0].description.smallDes}</p>
+          {serum.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>STEP SEVEN</p>
-          <p>{moisturiser.data[0].name}</p>
-          <p>£{moisturiser.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={moisturiser.data[0].imageLinks.img1}
-            alt={moisturiser.data[0].name}
-          />
-          <p className="iconDes">{moisturiser.data[0].description.smallDes}</p>
+          {moisturiser.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>OPTIONAL</p>
-          <p>{oil.data[0].name}</p>
-          <p>£{oil.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={oil.data[0].imageLinks.img1}
-            alt={oil.data[0].name}
-          />
-          <p className="iconDes">{oil.data[0].description.smallDes}</p>
+          {oil.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
         <div className="resultBox">
           <p>RECOMMENDED FOR AM USE.</p>
-          <p>{SPF.data[0].name}</p>
-          <p>£{SPF.data[0].price.toFixed(2)}</p>
-          <img
-            className="productpic"
-            src={SPF.data[0].imageLinks.img1}
-            alt={SPF.data[0].name}
-          />
-          <p className="iconDes">{SPF.data[0].description.smallDes}</p>
+          {SPF.data.map((value, key) => {
+            return (
+              <Fragment key={key}>
+                <p>{value.name}</p>
+                <p>£{value.price.toFixed(2)}</p>
+                <img
+                  className="productpic"
+                  src={value.imageLinks.img1}
+                  alt={value.name}
+                />
+                <p className="iconDes">{value.description.smallDes}</p>
+              </Fragment>
+            );
+          })}
           <button className="addButtons">Add to Wishlist</button>
           <button className="addButtons">Add to Cart</button>
         </div>
