9059f7105ce430ab350f6960cf785860c430ed7b
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-01-16 18:02:03 +0000

    loading css updates

--- glowstation/src/components/Loading.js
+++ glowstation/src/components/Loading.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Spinner from "react-bootstrap/Spinner";
 import "../css/loading.css";
 import { Redirect } from "react-router-dom";
 import "react-typewriting-effect/dist/index.css";
@@ -14,20 +13,11 @@ const Loading = (props) => {
   return (
     <div className="loadingP">
       <img src="../loading2.jpg" className="App-logo" alt="logo" />
-      <Spinner
-        className="cirlce"
-        animation="grow"
-        variant="primary"
-        role="status"
-      >
-        <span className="sr-only">Loading...</span>
-      </Spinner>
- 
       {redirect && <Redirect to={props.redirect} />}
     </div>
   );
 };
 
-// "/home" 
+// "/home"
 
 export default Loading;
