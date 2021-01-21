cf9e5e90848bf900ba91e22f83defe1ca5dcdf5e
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-01-21 23:02:34 +0000

    automatic search works on every component page

--- glowstation/src/components/Search/Search.js
+++ glowstation/src/components/Search/Search.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
-//import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "./Search.css";
 
 const Search = (props) => {
   const [keyword, setKeyword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = (event) => {
+    setRedirect(true)
     event.preventDefault();
     props.SearchBar(keyword);
   };
 
   return (
     <div className="searchArea">
+       {redirect && <Redirect to="/search" />}
       <div className="wholeSearch">
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="searchKeyword">
@@ -24,7 +27,7 @@ const Search = (props) => {
               onChange={(e) => setKeyword(e.target.value)}
             />
           </Form.Group>
-          
+
           <button className="searchB" type="submit">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -37,7 +40,6 @@ const Search = (props) => {
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
           </button>
-         
         </Form>
       </div>
     </div>
